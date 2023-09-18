b6dd91a1e3cd2d6f7e74766a6ca3b5e0359bd723
Johannes Handlechner <jhandlechner.lba@fh-salzburg.ac.at>
2023-09-18 22:25:45 +0200

    add strapi

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/strapi', '@nuxtjs/tailwindcss'],
+  strapi: {
+    url: process.env.STRAPI_URL || 'http://localhost:1337',
+    prefix: '/api',
+    version: 'v4',
+    cookie: {},
+    cookieName: 'strapi_jwt'
+  },
   devtools: { enabled: true },
   css: [
     "@/assets/fonts.css",
     "@/styles/main.scss",
     "@/styles/mixins.scss",
     "primevue/resources/themes/lara-light-blue/theme.css"
-  ],
-  modules: [
-      '@nuxtjs/tailwindcss'
   ],
   app: {
     pageTransition: { name: "page", mode: "out-in"},
