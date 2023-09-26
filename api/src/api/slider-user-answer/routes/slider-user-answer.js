499823ff9281bd67b91cf4f5273e7cff3133ab7a
tanja <tanjanicole.g@gmx.at>
2023-09-26 23:47:44 +0200

    feat: create core router for entities

--- api/src/api/slider-user-answer/routes/slider-user-answer.js
+++ api/src/api/slider-user-answer/routes/slider-user-answer.js
@@ -6,4 +6,20 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::slider-user-answer.slider-user-answer');
+// module.exports = createCoreRouter('api::slider-user-answer.slider-user-answer');
+module.exports = createCoreRouter('api::slider-user-answer.slider-user-answer', {
+    prefix: '',
+    only: ['find', 'findOne'],
+    except: [],
+    config: {
+      find: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+      findOne: {},
+      create: {},
+      update: {},
+      delete: {},
+    },
+});
--- api/src/api/survey-result/routes/survey-result.js
+++ api/src/api/survey-result/routes/survey-result.js
@@ -6,4 +6,21 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::survey-result.survey-result');
+//module.exports = createCoreRouter('api::survey-result.survey-result');
+module.exports = createCoreRouter('api::survey-result.survey-result', {
+    prefix: '',
+    only: ['find', 'findOne'],
+    except: [],
+    config: {
+      find: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+      findOne: {},
+      create: {},
+      update: {},
+      delete: {},
+    },
+});
+
