8655a9c0e87425da83937cfd9ca6de9513021339
Johannes Handlechner <jhandlechner.lba@fh-salzburg.ac.at>
2023-09-27 23:34:13 +0200

    feat(layout): change treeselect to dropdown

--- components/select.vue
+++ components/select.vue
@@ -1,22 +1,44 @@
 <template>
   <div>
     <label :for="props.name"><slot name="label"></slot></label>
-    <TreeSelect :id="props.name" v-model="selectedValue" :options="nodes" placeholder="Select Item" class="select" :pt="{
-      root: {
-        style: {
-          borderRadius: 0,
-          borderColor: 'black',
-          fontFamily: 'Cirka',
-          fontWeight: 200,
-          fontSize: '1.25rem'
+<!--    <TreeSelect :id="props.name" v-model="selectedValue" :options="nodes" placeholder="Select Item" class="select" :pt="{-->
+<!--      root: {-->
+<!--        style: {-->
+<!--          borderRadius: 0,-->
+<!--          borderColor: 'black',-->
+<!--          fontFamily: 'Cirka',-->
+<!--          fontWeight: 200,-->
+<!--          fontSize: '1.25rem'-->
+<!--        }-->
+<!--      }-->
+<!--    }" />-->
+      <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City" class="w-full md:w-14rem" :pt="{
+        root: {
+          style: {
+            borderRadius: 0,
+            borderColor: 'black',
+            fontFamily: 'Cirka',
+            fontWeight: 200,
+            fontSize: '1rem'
+          }
         }
-      }
-    }" />
+      }"
+      />
   </div>
 </template>
 
 <script setup>
 import TreeSelect from 'primevue/treeselect';
+import Dropdown from 'primevue/dropdown';
+
+const selectedCity = ref();
+const cities = ref([
+  { name: 'New York', code: 'NY' },
+  { name: 'Rome', code: 'RM' },
+  { name: 'London', code: 'LDN' },
+  { name: 'Istanbul', code: 'IST' },
+  { name: 'Paris', code: 'PRS' }
+]);
 
 const props = defineProps({
   name: String
