0546a41e16e4f41ddd7945b8bb91148d285fc322
Johannes Handlechner <jhandlechner.lba@fh-salzburg.ac.at>
2024-02-17 19:23:31 +0100

    rightbar

--- components/content/survey-welcome/right-square-content.vue
+++ components/content/survey-welcome/right-square-content.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <p class="flex flex-wrap flex-column">
-      (c) <u class="me-1 ms-1"><nuxt-link external to="https://prio.kollektivschlaflos.com" target="_blank">kollektiv schlaflos, </nuxt-link></u>
-      <u class="me-1"><nuxt-link external to="https://www.linkedin.com/in/johannes-handlechner/" target="_blank">jo handlechner</nuxt-link></u> <span class="me-1">&</span>
-      <u><nuxt-link external to="https://www.linkedin.com/in/tanjanicole/" target="_blank">tanja gruber</nuxt-link></u>
+    <p class="flex flex-wrap flex-col">
+      <span>
+        (c) <u class="me-1 ms-1"><nuxt-link external to="https://prio.kollektivschlaflos.com" target="_blank">kollektiv schlaflos, </nuxt-link></u>
+      </span>
+      <span class="flex flex-wrap">
+        <u class="me-1"><nuxt-link external to="https://www.linkedin.com/in/johannes-handlechner/" target="_blank">jo handlechner</nuxt-link></u> <span class="me-1">&</span>
+        <u><nuxt-link external to="https://www.linkedin.com/in/tanjanicole/" target="_blank">tanja gruber</nuxt-link></u>
+      </span>
     </p>
   </div>
 
